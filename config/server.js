3e37702c1fbcf33e09ea5b0be5b778878de50a4b
Anthony Lozano <lozanoj914@gmail.com>
2022-02-27 01:55:59 -0500

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,3 +1,8 @@
 module.exports = ({ env }) => ({
+  host: env("DATABASE_HOST"),
+  port: env.int("DATABASE_PORT"),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
   url: env("MY_HEROKU_URL"),
 });
